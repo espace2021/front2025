97020acafd82c842ead2af00426530759d92453e
hsan-isetsf <hsan.isetsf@gmail.com>
2025-03-25 18:29:01 +0100

    first-commit

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import "./card.css"
+const Card = ({imagepro,title,prix}) => {
+  return (
+    <div className='card'>
+        {imagepro && <img src={imagepro} alt={title} />}
+        <div className='card-content'>
+            <h1 className='card-title'>{title}</h1>
+            
+            <h1 className='card-title'>Prix : {prix}TND</h1>
+            <button className="card-button"><i className="fa-solid fa-basket-shopping"></i>Add to card</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Card
---
+++ src/pages/ProductCard.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import Card from "../components/card/Card";
+import {fetchproduits} from "../service/produitservice";
+
+
+const ProductCard = () => {
+  const [produits, setProduit] = useState([]);
+  const fetchProducts = async () => {
+    try {
+      const res = await fetchproduits();
+      setProduit(res.data);
+
+    }catch (error){
+      console.log(error);
+    }
+  };
+useEffect(()=>{
+  fetchProducts();
+},[]);
+  return (
+    <div className="card-container justify-content-center">
+      {produits.map((pro,index)=>
+      <Card 
+      key={index}
+      imagepro={pro.imagepro}
+      title={pro.title}
+      
+      prix={pro.prix}
+      />
+    )}
+      
+    </div>
+  )
+}
+
+export default ProductCard
